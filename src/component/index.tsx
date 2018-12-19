98527deb44f6aa591654c32111f0c3d29391b891
Donald L Mull Jr <donald.mull@gmail.com>
2018-12-19 00:44:49 -0500

    hello world

--- src/component/index.tsx
+++ src/component/index.tsx
@@ -1,17 +1,28 @@
-import Openseadragon from 'openseadragon';
+import * as Openseadragon from 'openseadragon';
 import * as React from 'react';
-
-Openseadragon();
 
 export interface OpenSeadragonComponentProps {
     url: string;
+    containerStyle?: React.CSSProperties;
 }
 
 export default class OpenSeadragonComponent extends React.Component<OpenSeadragonComponentProps> {
+    private containerElementRef = React.createRef<HTMLDivElement>();
+
+    public componentDidMount() {
+        Openseadragon({
+            element: this.containerElementRef.current,
+            sequenceMode: true,
+            tileSources: [
+                this.props.url
+            ]
+        });
+    }
 
     public render() {
-        return <div>
-            Placeholder for {this.props.url}
-        </div>;
+        return <div
+            ref={this.containerElementRef}
+            style={this.props.containerStyle}
+        />;
     }
 }
